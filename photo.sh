ef71f24890462c05a35f53b5cf1aaed5e45817e5
D. Scott Boggs <dscottboggs@gmail.com>
2015-12-18 12:08:13 -0500

    Fully working if there are pictures on the camera. Ugly, no error checking.

--- photo.sh
+++ photo.sh
@@ -4,12 +4,12 @@
 # all files are redirected to ~/Pictures/test instead of ~/Pictures until done
 #		     ^^ Don't forget to fix that ^^
 #
-#	v0.0.2 created 2015-12-18
+#	v0.0.3 created 2015-12-18
 #	D. Scott Boggs
 
 
 currentDate=$(date +"%Y-%m-%d")		#save current date for filename
-dirName=$"Canon-Backup-$currentDate/"
+dirName=$"Canon-Backup-$currentDate"
 dirLongName=$"$PWD/$dirName"
 echo "Creating folder $dirName..."
 sudo mkdir $dirLongName   #create folder for today's dump
@@ -23,3 +23,8 @@ sudo gphoto2 --get-all-files		#get all files and save to new dir
 echo "creating folder $dirName/TIFF8 for GIMP-compatible, uncompressed TIFFs..."
 sudo mkdir $dirLongName/TIFF8/		#create a subdirectory for GIMP compatible TIFFs
 echo "converting files..."
+sudo rawtherapee -o TIFF8/ -b8 -t -c *.CR2
+echo "making extra directories for later organization..."
+sudo mkdir $dirLongName/GIMP_files/
+sudo mkdir $dirLongName/Renders
+echo "done."
